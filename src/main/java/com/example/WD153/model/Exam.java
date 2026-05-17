41b994273e9fa0c18a7fa60e2d3598be4fc48272
IT25100336 <it25100336@my.sliit.lk>
2026-05-17 16:03:40 +0530

    Add exam java file to model package
    It stores exam-related details of a student, including student email, course code, exam name, marks obtained, and maximum marks. The class uses constructors, getters, and setters to manage data efficiently. It also includes a getPercentage() method to calculate the student’s exam percentage automatically based on marks and maximum marks.

---
+++ src/main/java/com/example/WD153/model/Exam.java
@@ -0,0 +1,39 @@
+package com.example.WD153.model;
+
+public class Exam {
+    private String studentEmail;
+    private String courseCode;
+    private String examName;
+    private double marks;
+    private double maxMarks;
+
+    public Exam() {}
+
+    public Exam(String studentEmail, String courseCode, String examName, double marks, double maxMarks) {
+        this.studentEmail = studentEmail;
+        this.courseCode = courseCode;
+        this.examName = examName;
+        this.marks = marks;
+        this.maxMarks = maxMarks;
+    }
+
+    public String getStudentEmail() { return studentEmail; }
+    public void setStudentEmail(String studentEmail) { this.studentEmail = studentEmail; }
+
+    public String getCourseCode() { return courseCode; }
+    public void setCourseCode(String courseCode) { this.courseCode = courseCode; }
+
+    public String getExamName() { return examName; }
+    public void setExamName(String examName) { this.examName = examName; }
+
+    public double getMarks() { return marks; }
+    public void setMarks(double marks) { this.marks = marks; }
+
+    public double getMaxMarks() { return maxMarks; }
+    public void setMaxMarks(double maxMarks) { this.maxMarks = maxMarks; }
+
+   
+    public double getPercentage() {
+        return (marks / maxMarks) * 100;
+    }
+}
