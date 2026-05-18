daab6b83d735cfe385aaf073de6b23ee8802c9f2
IT25102405 <IT25102405@my.sliit.lk>
2026-05-18 19:15:04 +0530

    Update  again DashboardController.java

--- src/main/java/com/example/WD153/controller/DashboardController.java
+++ src/main/java/com/example/WD153/controller/DashboardController.java
@@ -15,7 +15,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
+/**
+ * DashboardController aggregates various models (User, Course, Exam, Enrollment)
+ * to populate the complex views required by the dashboard.
+ */
 
 @Controller
 public class DashboardController {
